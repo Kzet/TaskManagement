db62e58a3b180ded963bf536b1c79939e50a33a5
artur <protivih@mail.ru>
2025-06-26 10:00:47 +0300

    angular add logout

--- Presentation/src/app/layouts/main-layout/main-layout.component.ts
+++ Presentation/src/app/layouts/main-layout/main-layout.component.ts
@@ -3,13 +3,24 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { AuthService } from '../../services/auth.service';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule],
+  imports: [RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule, NzGridModule,
+    NzButtonModule],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss'
 })
 export class MainLayoutComponent {
   isCollapsed = false;
+
+  constructor(private authService: AuthService) {
+
+  }
+  logOut() {
+    this.authService.logout();
+  }
 }
